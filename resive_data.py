9e55610da696b61009b31b3c5929554e32d007e5
jaye1944 <jaye1944@gmail.com>
2013-07-24 18:44:23 +0530

    First commit

---
+++ resive_data.py
@@ -0,0 +1,50 @@
+import random
+global ffname
+global sq
+
+global error_ratio
+error_ratio = 0.3
+
+
+def error_or_not():
+    ra = random.randrange(0,int(sq))
+    if(ra < sq*error_ratio):
+        return True
+    return False
+
+def error(dict,seqnumber):    #simple error checking
+    for i,j in dict.items():
+        for p,q in j.items():
+            if( p == q.__hash__() and (seqnumber == i)):#check error using hash value and fram
+               # simulation error using uniform distributuion
+                if(error_or_not()):
+                   return False
+                return True
+    return False
+
+def writer(or_file):
+    f = open(ffname, 'wb')
+    for i in or_file:
+        f.write(i)
+    f.close()
+   # os.remove(f)
+
+def datapart(dict):
+    for i,j in dict.items():
+        for p,q in j.items():
+            return q
+
+def window_number(dict):
+    for i,j in dict.items():
+        return i
+
+def print_All(time,errors,noterrors):
+    print("\n------- Results-------")
+    print("Time " + time)
+    print("File resived compleatly")
+    print("Error packets resived " + str(errors))
+    print("Error free resived "+ str(noterrors))
+    print("Total packets "+ str(errors+noterrors))
+    print("Calcualated Error ratio "+ str(errors/(errors+noterrors)))
+    print("Original error "+ str(error_ratio))
+
---
+++ sttob.py
@@ -0,0 +1,25 @@
+
+from threading import Thread
+from time import sleep
+
+def threaded_function(arg):
+    #for i in range(arg):
+    while True:
+        print("running")
+        sleep(1)
+
+def stop_function(arg):
+    while True:
+        id = input("Do you want to stop : ")
+        if (id == "y"):
+            arg._stop()
+            break
+        print("while loop")
+
+
+#if __name__ == "__main__":
+thread1 = Thread(target = threaded_function, args = (100, ))
+thread = Thread(target = stop_function, args = (thread1, ))
+thread1.start()
+
+thread.start()
